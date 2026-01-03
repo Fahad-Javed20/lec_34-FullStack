4214a6c7f1b696c7b786aa1b3a5a87a70049cb91
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-04 00:14:03 +0500

    make chnages

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,13 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL || "");
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("falled to Connet DB", error);
+    process.exit(1);
+  }
+};
 
-    try{
-
-    }
-}
+export default connectDB
