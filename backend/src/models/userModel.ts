bf1de74f2b7db2bc644a249cd5753d06071950de
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-10 08:48:17 +0500

    make chnges

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastname: { type: String, required: true },
     email: { type: String, required: true },
-    salary: { type: Number, required: true },
-    address: { type: String, required: true },
-    country: { type: String, required: true },
-    city: { type: String, required: true },
+    password: { type: String, required: true },
+    gender: { type: String, required: true },
   },
   { timestamps: true }
 );
