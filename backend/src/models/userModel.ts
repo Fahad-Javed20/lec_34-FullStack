bbc8d7f4324818a8f460c83796c4c50c6f801d6f
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-10 09:10:16 +0500

    Update userModel.ts

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -7,8 +7,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     gender: { type: String, required: true },
-  },
-  { timestamps: true }
+  }
 );
 
 const User = mongoose.model("User", userSchema);
